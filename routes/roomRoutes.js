4b3a5d719bf3315ead634bc24af8102772423d6d
Zack <muhzaki46@gmail.com>
2025-11-22 18:49:52 +0700

    fix agar stok kembali stetlah jam

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -8,7 +8,7 @@ router.get('/', requireAuth, roomController.getRooms);
 
 // Update Status (Maintenance)
 router.patch('/:id/status', requireAuth, requireRole(['admin']), roomController.updateRoomStatus);
-///
+
 // --- BARU: Update Data (Edit Info) ---
 router.put('/:id', requireAuth, requireRole(['admin']), roomController.updateRoom);
 
