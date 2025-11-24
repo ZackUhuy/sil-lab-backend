d391f7079a41471fa9613344ece4288eb73e9d87
Zack <muhzaki46@gmail.com>
2025-11-24 22:55:08 +0700

    menambahkan fitur reset pw di auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { requireAuth } = require('../middleware/authMiddleware');
 
-// Endpoint untuk Register
+// Endpoint Register & Login
 router.post('/register', authController.register);
-
-// Endpoint untuk Login
 router.post('/login', authController.login);
+
+// --- INI PENTING: Reset Password ---
+// Pastikan baris ini ada!
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/update-password', requireAuth, authController.updateUserPassword);
 
 module.exports = router;
